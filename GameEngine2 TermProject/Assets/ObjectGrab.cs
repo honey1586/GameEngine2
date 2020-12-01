01e0318127069ab1966fc291712d11cb885ce3b6
SBS <llkey12@naver.com>
2020-12-02 02:46:33 +0900

    핸드 그랩 스크립트 수정 및 공함정 낙하 함정 구현

--- GameEngine2 TermProject/Assets/ObjectGrab.cs
+++ GameEngine2 TermProject/Assets/ObjectGrab.cs
@@ -4,61 +4,48 @@ using UnityEngine;
 
 public class ObjectGrab : MonoBehaviour
 {
-    private Rigidbody _rigid;
+    public Rigidbody objrigid;
 
     public Transform player;
+    
+    
 
-    public Transform playerHand;
-
-    public float throwForce = 10;
-
-    private bool isRanged = false;
+    private bool IsGrabable = false;
 
     private bool isGrabed = false;
     
     // Start is called before the first frame update
     void Awake()
     {
-        _rigid = GetComponent<Rigidbody>();
+        objrigid = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        isInRange();
+
+    }
+
+    public void isInRange()
+    {
         float dist = Vector3.Distance(gameObject.transform.position, player.position);
 
         if (dist < 2.5f)
         {
-            isRanged = true;
+            IsGrabable = true;
         }
         else
         {
-            isRanged = false;
+            IsGrabable = false;
         }
+  
+    }
 
-
-        if (isRanged && Input.GetMouseButtonDown(0))
-        {
-            _rigid.isKinematic = true;
-            transform.parent = playerHand;
-            isGrabed = true;
-        }
-
-        if (isGrabed)
-        {
-            if (Input.GetMouseButtonUp(0))
-            {
-                _rigid.isKinematic = false;
-                transform.parent = null;
-                isGrabed = false;
-            }
-
-            if (Input.GetKeyDown(KeyCode.F))
-            {
-                _rigid.isKinematic = false;
-                transform.parent = null;
-                _rigid.AddForce(playerHand.forward*throwForce,ForceMode.Impulse);
-            }
-        }
+  
+    public bool GetIsGrabable()
+    {
+        return IsGrabable;
     }
 }
---
+++ GameEngine2 TermProject/Assets/Script/FallingTrap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingTrap : MonoBehaviour
+{
+    public Rigidbody trapRigidBody;
+    public float fallingSpeed = 50;
+    private void OnTriggerEnter(Collider other)
+    {
+        trapRigidBody.isKinematic = false;
+        trapRigidBody.AddForce(Vector3.down*fallingSpeed,ForceMode.Impulse);
+    }
+}
